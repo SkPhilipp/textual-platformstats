6ca973f9c688436b6d3b050aab674a1b0dfe28c8
Philipp Gayret <philipp@release-engineers.com>
2024-05-19 18:50:19 +0200

    feat: Added application option

--- README.md
+++ README.md
@@ -2,11 +2,19 @@
 
 Get stats on usage of your development platform tooling.
 
+## Development
+
+1. Install [Python 3.12](https://www.python.org/downloads/).
+2. Install dependencies with `pip install -r requirements.txt`.
+3. Run the tool via `python main.py`.
+
+And last but not least; set up the tools you want to test and follow the instructions below to extract their logs.
+
 ## Artifactory
 
 - Create a support bundle in Artifactory's `/ui/admin/support-zone`.
 - Download it.
 
 ```bash
-platformstats "${SUPPORT_BUNDLE}.zip"
+platformstats artifactory "${SUPPORT_BUNDLE}.zip"
 ```
--- main.py
+++ main.py
@@ -7,19 +7,24 @@ from tools.artifactory.unpack import unpack
 
 def main():
     parser = argparse.ArgumentParser(description='Get platform insights.')
+    parser.add_argument('application', help='The application to analyze.')
     parser.add_argument('zipfile', help='The path to a zip file.')
 
     args = parser.parse_args()
 
-    log_files = unpack(args.zipfile)
-    print(f'Found {len(log_files)} log files.')
-    router_request_log_files = [file for file in log_files if 'router-request.log' in file]
-    print(f'Found {len(router_request_log_files)} "router-request.log" files.')
-    aggregator = Aggregator()
-    print('Loading log files...')
-    for log_file in router_request_log_files:
-        aggregator.parse_router_request_log(log_file)
-    aggregator.summarize_router_request_data()
+    if args.application == 'artifactory':
+        log_files = unpack(args.zipfile)
+        print(f'Found {len(log_files)} log files.')
+        router_request_log_files = [file for file in log_files if 'router-request.log' in file]
+        print(f'Found {len(router_request_log_files)} "router-request.log" files.')
+        aggregator = Aggregator()
+        print('Loading log files...')
+        for log_file in router_request_log_files:
+            aggregator.parse_router_request_log(log_file)
+        aggregator.summarize_router_request_data()
+        return
+
+    print(f'Unknown application: {args.application}')
 
 
 if __name__ == "__main__":
